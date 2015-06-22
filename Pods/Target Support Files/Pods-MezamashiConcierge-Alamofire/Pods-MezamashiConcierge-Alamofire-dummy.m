d9be4455bb2207c1fd96f50b12f87ea5407ecb43
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-06-23 02:15:17 +0900

    Pod install and install alamofire

---
+++ Pods/Target Support Files/Pods-MezamashiConcierge-Alamofire/Pods-MezamashiConcierge-Alamofire-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MezamashiConcierge_Alamofire : NSObject
+@end
+@implementation PodsDummy_Pods_MezamashiConcierge_Alamofire
+@end
---
+++ Pods/Target Support Files/Pods-MezamashiConciergeTests-Alamofire/Pods-MezamashiConciergeTests-Alamofire-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MezamashiConciergeTests_Alamofire : NSObject
+@end
+@implementation PodsDummy_Pods_MezamashiConciergeTests_Alamofire
+@end
