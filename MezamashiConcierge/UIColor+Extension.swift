e4b303751c5446b287c77779d0090b5fcf1545b1
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-06-23 11:26:32 +0900

    Create UIColor extension

---
+++ MezamashiConcierge/UIColor+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+Extension.swift
+//  MezamashiConcierge
+//
+//  Created by shinbo maiki on 2015/06/23.
+//  Copyright (c) 2015年 shinbo maiki. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func color(#decRed: CGFloat, decGreen: CGFloat, decBlue: CGFloat, alpha: CGFloat) -> UIColor {
+        return UIColor(red: decRed/255.0, green: decGreen/255.0, blue: decBlue/255.0, alpha: alpha)
+    }
+    class func mainColor() -> UIColor {
+        return self.color(decRed: 0, decGreen: 0, decBlue: 0, alpha: 1)
+    }
+    class func subColor01() -> UIColor {
+        return self.color(decRed: 255, decGreen: 255, decBlue:2550, alpha: 1)
+    }
+    class func subColor02() -> UIColor {
+        return self.color(decRed: 218, decGreen: 165, decBlue: 32, alpha: 1)
+    }
+}
+
