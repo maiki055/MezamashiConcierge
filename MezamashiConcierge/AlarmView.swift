89d2fa7079d2f251ede3dafe79b7ac6aec1eb3b6
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-06-23 13:21:31 +0900

    Create alarm view

---
+++ MezamashiConcierge/AlarmView.swift
@@ -0,0 +1,22 @@
+//
+//  AlarmView.swift
+//  MezamashiConcierge
+//
+//  Created by shinbo maiki on 2015/06/23.
+//  Copyright (c) 2015年 shinbo maiki. All rights reserved.
+//
+
+import UIKit
+
+class AlarmView: UIView {
+    let alarm = Alarm()
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
