1340bd741e6f340241c600eeccd82b70cd27772e
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-06-25 17:36:47 +0900

    Make messageView from source code to using xib

--- MezamashiConcierge/MessageView.swift
+++ MezamashiConcierge/MessageView.swift
@@ -9,43 +9,34 @@
 import UIKit
 
 class MessageView: UIView {
-    let messageLabel = UILabel()
     let underlineView = UIView()
-    let subMessageLabel = UILabel()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var jaMessageLabel: UILabel!
+    @IBOutlet weak var enMessageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
         
-        let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: 60))
-        titleLabel.text = "Concierge"
         titleLabel.font = UIFont.alphanumericFont(30)
         titleLabel.textColor = UIColor.subColor01()
-        titleLabel.textAlignment = NSTextAlignment.Center
         
-        messageLabel.font = UIFont.alphanumericFont(20)
-        messageLabel.textColor = UIColor.subColor01()
+        enMessageLabel.font = UIFont.alphanumericFont(20)
+        enMessageLabel.textColor = UIColor.subColor01()
         
-        messageLabel.text = "not setting alarm."
-        messageLabel.sizeToFit()
-        messageLabel.frame.origin = CGPoint(x: (self.frame.width - messageLabel.frame.width) / 2, y: titleLabel.frame.maxY)
+//        enMessageLabel.text = "not setting alarm."
+        enMessageLabel.text = "setting alarm."
+        enMessageLabel.sizeToFit()
         
         underlineView.backgroundColor = UIColor.subColor01()
-        underlineView.frame = CGRectMake(0, messageLabel.frame.height - 4, messageLabel.frame.width, 0.3)
+        underlineView.frame = CGRectMake(0, enMessageLabel.frame.height - 4, enMessageLabel.frame.width, 0.3)
         underlineView.autoresizingMask = .FlexibleRightMargin | .FlexibleBottomMargin | .FlexibleLeftMargin
-
-        subMessageLabel.frame = CGRect(x: 0, y: messageLabel.frame.maxY + 8, width: self.frame.width, height:20)
-        subMessageLabel.text = "アラームは設定されていません。"
-        subMessageLabel.font = UIFont.font(12)
-        subMessageLabel.textColor = UIColor.subColor01()
-        subMessageLabel.textAlignment = NSTextAlignment.Center
         
-        self.addSubview(titleLabel)
-        self.addSubview(messageLabel)
-        messageLabel.addSubview(underlineView)
-        self.addSubview(subMessageLabel)
-    }
-
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+//        jaMessageLabel.text = "アラームは設定されていません。"
+        jaMessageLabel.text = "明日の08:50にアラームが設定されています。"
+        jaMessageLabel.font = UIFont.font(12)
+        jaMessageLabel.textColor = UIColor.subColor01()
+        
+        enMessageLabel.addSubview(underlineView)
     }
 }
--- MezamashiConcierge/ViewController.swift
+++ MezamashiConcierge/ViewController.swift
@@ -17,7 +17,9 @@ class ViewController: UIViewController {
         self.view.backgroundColor = UIColor.mainColor()
         
         alarmView = ClockView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.width))
-        messageView = MessageView(frame: CGRect(x: 0, y: alarmView.frame.maxY, width: self.view.frame.width, height: self.view.frame.height - alarmView.frame.maxY))
+//        messageView = MessageView(frame: CGRect(x: 0, y: alarmView.frame.maxY, width: self.view.frame.width, height: self.view.frame.height - alarmView.frame.maxY))
+        messageView = UINib(nibName: "MessageView", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! MessageView
+        messageView.frame = CGRect(x: 0, y: alarmView.frame.maxY, width: self.view.frame.width, height: self.view.frame.height - alarmView.frame.maxY)
         self.view.addSubview(alarmView)
         self.view.addSubview(messageView)
         
