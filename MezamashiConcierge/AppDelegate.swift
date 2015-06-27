d28f366b1c4359c47da413e2652647f9e1039bd6
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-06-27 10:35:05 +0900

    introduce Reachability

--- MezamashiConcierge/AppDelegate.swift
+++ MezamashiConcierge/AppDelegate.swift
@@ -13,14 +13,17 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var networkConnectivity: NetworkConnectivity?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         UINavigationBar.appearance().tintColor = UIColor.color(decRed: 80, decGreen: 80, decBlue: 80, alpha: 1)
+        
+        networkConnectivity = NetworkConnectivity()
         // Override point for customization after application launch.
         return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -36,6 +39,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
+        networkConnectivity?.checkReachability()
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
---
+++ MezamashiConcierge/NetworkConnectivity.swift
@@ -0,0 +1,55 @@
+//
+//  NetworkConnectivity.swift
+//  MezamashiConcierge
+//
+//  Created by shinbo maiki on 2015/06/27.
+//  Copyright (c) 2015年 shinbo maiki. All rights reserved.
+//
+
+import UIKit
+
+class NetworkConnectivity: NSObject {
+    let reachability = MCReachability.reachabilityForInternetConnection()
+    var isReachable = false
+    
+    override init() {
+        super.init()
+        
+        startNotifier()
+    }
+    
+    func checkReachability() {
+        if reachability.isReachable() {
+            notifyReachable()
+        } else {
+            notifyNotReachable()
+        }
+    }
+    
+    func startNotifier() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityChanged:", name: ReachabilityChangedNotification, object: reachability)
+        reachability.startNotifier()
+    }
+    
+    func reachabilityChanged(note: NSNotification) {
+        if reachability.isReachable() {
+            notifyReachable()
+        }
+        else {
+            notifyNotReachable()
+        }
+    }
+    
+    func notifyReachable() {
+        isReachable = true
+    }
+    
+    func notifyNotReachable() {
+        isReachable = false
+        let alertController = UIAlertController(title: "ネットワークに接続されていません", message: "ネットワーク接続がない状態では、天候の情報や運行情報を取得できません", preferredStyle: .Alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(defaultAction)
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        appDelegate.window?.rootViewController?.presentViewController(alertController, animated: true, completion: nil)
+    }
+}
