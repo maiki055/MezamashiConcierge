9837a776512a09101c64f9ee4e1b222478ee6332
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-07-10 00:54:06 +0900

    Create Area View Controller

---
+++ MezamashiConcierge/AreaViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AreaViewController.swift
+//  MezamashiConcierge
+//
+//  Created by shinbo maiki on 2015/07/10.
+//  Copyright (c) 2015年 shinbo maiki. All rights reserved.
+//
+
+import UIKit
+
+class AreaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Area"
+        view.backgroundColor = UIColor.mainColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
