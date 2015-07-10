7cd3c0a692f52f2b71a77b2b1ca7293fd155377c
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-07-10 19:12:08 +0900

    Introduce MRProgress by pod

--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@ use_frameworks!
 
 pod 'Alamofire', '~> 1.2.0'
 pod 'SwiftyJSON', '~> 2.2.0'
+pod 'MRProgress'
 
 target 'MezamashiConcierge' do
 end
---
+++ Pods/Target Support Files/Pods-MezamashiConciergeTests-MRProgress/Pods-MezamashiConciergeTests-MRProgress-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MezamashiConciergeTests_MRProgress : NSObject
+@end
+@implementation PodsDummy_Pods_MezamashiConciergeTests_MRProgress
+@end
